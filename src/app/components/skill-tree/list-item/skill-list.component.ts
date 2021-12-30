45051579874e8f88b0cb359752ada87c683d552b
ViewableGravy <lleyton92@gmail.com>
2021-12-30 21:04:54 +1100

    [infinite-scroll] Created basic timeline

---
+++ src/app/components/skill-tree/list-item/skill-list.component.ts
@@ -0,0 +1,77 @@
+import { AfterViewInit, Component, Input, NgModule, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+
+@Component({
+  selector: 'skill-listcomponent',
+  templateUrl: './skill-list.component.html',
+  styleUrls: ['./skill-list.component.scss']
+})
+export class SkillListComponent implements AfterViewInit {
+    items = [
+        {
+            heading: "eleventh Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "tenth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "nineth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "Eighth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "seventh Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "Sixth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "Fifth Cardd",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "Fourth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "Third Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "Second Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "first card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        }
+    ];
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+    ngAfterViewInit(): void {
+        
+    }
+}
+
+@NgModule({
+    imports: [
+        BrowserModule
+    ],
+    exports: [SkillListComponent],
+    declarations: [SkillListComponent],
+    providers: [],
+})
+  
+export class SkillListModule {}
--- src/app/components/skill-tree/skill-tree.component.ts
+++ src/app/components/skill-tree/skill-tree.component.ts
@@ -1,20 +1,52 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Component, OnInit, HostListener, NgModule, ViewChild, ElementRef, ChangeDetectorRef, ChangeDetectionStrategy, AfterViewInit } from '@angular/core';
+import { SkillListModule } from './list-item/skill-list.component'
 
 @Component({
   selector: 'app-skill-tree',
   templateUrl: './skill-tree.component.html',
   styleUrls: ['./skill-tree.component.scss']
 })
-export class SkillTreeComponent implements OnInit {
+export class SkillTreeComponent implements AfterViewInit {
 
-  constructor() { }
+  translation = 0;
+
+  @ViewChild('container') public container: ElementRef;
+
+  constructor() { 
+  }
 
   ngOnInit(): void {
+    this.scrollBottom();
+  }
+
+  ngAfterViewInit(): void {
+    this.scrollBottom();
+  }
+
+  scrollBottom(): void {
+    try {
+      this.container.nativeElement.scrollTop = this.container.nativeElement.scrollHeight;
+    } catch (err) {}  
   }
 
   @HostListener('mousewheel', ['$event']) scroll(event: any) {
-    let wheelDelta = Math.max(-1, Math.min(1, (event.deltaY || -event.detail)));
-    wheelDelta === 1 ? this.MoveDown() : this.MoveUp();
+    if (document.getElementById('tree').classList.contains("focused")) {
+      //this.translation += event.deltaY / 3;
+      //document.getElementById("timeline").style.transform = `translateY(${this.translation}px)`
+    } else {
+      let wheelDirection = Math.max(-1, Math.min(1, (event.deltaY || -event.detail)));
+      
+      //wheelDelta === 1 ? this.MoveDown() : this.MoveUp();
+      if (wheelDirection === 1) {
+        this.MoveDown();
+      }
+    }
+  }
+
+  updateScroll(){
+    var element = document.getElementById("timeline");
+    element.scrollTop = -element.scrollHeight;
   }
 
   onSwipe(event) {
@@ -23,7 +55,7 @@ export class SkillTreeComponent implements OnInit {
       if (event.deltaY < 0) {
         this.MoveDown();
       } else {
-        this.MoveUp();
+        //this.MoveUp();
       }
     }
   }
