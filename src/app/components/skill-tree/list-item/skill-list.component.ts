6b85dd6cf8ce7e2fdaa380ccc5cce12f1d808cde
ViewableGravy <lleytonmorris@Lleytons-MacBook-Pro.local>
2022-01-01 12:16:42 +1100

    [infinite-scroll] Implemented the infinite scrolling (for standard heights)

--- src/app/components/skill-tree/list-item/skill-list.component.ts
+++ src/app/components/skill-tree/list-item/skill-list.component.ts
@@ -1,40 +1,204 @@
-import { AfterViewInit, Component, Input, NgModule, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, NgModule, OnInit, ViewChild, QueryList, ViewChildren, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'skill-listcomponent',
   templateUrl: './skill-list.component.html',
-  styleUrls: ['./skill-list.component.scss']
+  styleUrls: ['./skill-list.component.scss'],
 })
 export class SkillListComponent implements AfterViewInit {
+    @ViewChildren('cards') public cards: QueryList<ElementRef>;
+
+    //Local "cache" of cards. Not all are loaded however this means that scrolling quickly should be smooth af
+    //This will get updated less frequently from a database but will get more data at a time to put it into memory
     items = [
+        {
+            heading: "fourteenth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "fourteenth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "thirteenth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "twelth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "fourteenth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "fourteenth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "thirteenth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "twelth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "fourteenth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "fourteenth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "thirteenth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "twelth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "fourteenth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "fourteenth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "thirteenth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "twelth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "fourteenth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "fourteenth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "thirteenth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "twelth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "fourteenth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "fourteenth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "thirteenth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "twelth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "fourteenth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "fourteenth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "thirteenth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "twelth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "fourteenth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "fourteenth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "thirteenth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "twelth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "fourteenth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "fourteenth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "thirteenth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "twelth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "fourteenth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "fourteenth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "thirteenth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
+        {
+            heading: "twelth Card",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        },
         {
             heading: "eleventh Card",
             body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
         },
         {
             heading: "tenth Card",
-            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+            body: "dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
         },
         {
             heading: "nineth Card",
-            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+            body: "Lorem ipsum dolor sit amet"
         },
         {
             heading: "Eighth Card",
-            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+            body: "ex ea commodo consequat"
         },
         {
             heading: "seventh Card",
-            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+            body: "labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
         },
         {
             heading: "Sixth Card",
             body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
         },
         {
-            heading: "Fifth Cardd",
+            heading: "Fifth Card",
             body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
         },
         {
@@ -43,25 +207,67 @@ export class SkillListComponent implements AfterViewInit {
         },
         {
             heading: "Third Card",
-            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+            body: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
         },
         {
             heading: "Second Card",
-            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+            body: "incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"
         },
         {
             heading: "first card",
-            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
         }
     ];
+
+    min = 8;
+    max = 15;
 
     constructor() { }
 
     ngOnInit(): void {
+        this.max = this.items.length;
+        this.min = this.max - 8;
     }
 
     ngAfterViewInit(): void {
-        
+    }
+
+
+//learn how to force re-render
+    public loadNewTops(): boolean {
+        if (this.min >= 2) {
+            this.min -= 2;
+            return true;
+        }
+
+        return false;
+    }
+
+    public cullBottom(): boolean {
+        if (this.max > 1) {
+            this.max--;
+            return true;
+        }
+
+        return false;
+    }
+
+    public loadNewBottom(): boolean {
+        if (this.max < this.items.length) {
+            this.max++;
+            return true;
+        }
+
+        return false;
+    }
+
+    public cullTop(): boolean {
+        if (this.min < this.items.length + 1) {
+            this.min += 2;
+            return true;
+        }
+
+        return false;
     }
 }
 
@@ -73,5 +279,5 @@ export class SkillListComponent implements AfterViewInit {
     declarations: [SkillListComponent],
     providers: [],
 })
-  
+
 export class SkillListModule {}
--- src/app/components/skill-tree/skill-tree.component.ts
+++ src/app/components/skill-tree/skill-tree.component.ts
@@ -1,6 +1,6 @@
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
-import { Component, OnInit, HostListener, NgModule, ViewChild, ElementRef, ChangeDetectorRef, ChangeDetectionStrategy, AfterViewInit } from '@angular/core';
-import { SkillListModule } from './list-item/skill-list.component'
+import { Component, OnInit, HostListener, NgModule, ViewChild, ElementRef, ChangeDetectorRef, ChangeDetectionStrategy, AfterViewInit, QueryList } from '@angular/core';
+import { SkillListModule, SkillListComponent } from './list-item/skill-list.component'
 
 @Component({
   selector: 'app-skill-tree',
@@ -8,10 +8,11 @@ import { SkillListModule } from './list-item/skill-list.component'
   styleUrls: ['./skill-tree.component.scss']
 })
 export class SkillTreeComponent implements AfterViewInit {
-
-  translation = 0;
-
   @ViewChild('container') public container: ElementRef;
+  @ViewChild(SkillListComponent) private skillList: SkillListComponent;
+
+  private topCard;
+  private bottomCard;
 
   constructor() { 
   }
@@ -24,29 +25,81 @@ export class SkillTreeComponent implements AfterViewInit {
     this.scrollBottom();
   }
 
-  scrollBottom(): void {
-    try {
-      this.container.nativeElement.scrollTop = this.container.nativeElement.scrollHeight;
-    } catch (err) {}  
-  }
-
-  @HostListener('mousewheel', ['$event']) scroll(event: any) {
+  @HostListener('mousewheel', ['$event']) 
+  scroll(event: any) {
     if (document.getElementById('tree').classList.contains("focused")) {
-      //this.translation += event.deltaY / 3;
-      //document.getElementById("timeline").style.transform = `translateY(${this.translation}px)`
+      //timeline
     } else {
       let wheelDirection = Math.max(-1, Math.min(1, (event.deltaY || -event.detail)));
-      
-      //wheelDelta === 1 ? this.MoveDown() : this.MoveUp();
       if (wheelDirection === 1) {
         this.MoveDown();
       }
     }
   }
 
-  updateScroll(){
-    var element = document.getElementById("timeline");
-    element.scrollTop = -element.scrollHeight;
+  timelineScroll(event) {
+    if (!document.getElementById('tree').classList.contains("focused")) {
+      return;
+    }
+
+    this.skillList.cards.forEach((c) => {
+      c.nativeElement.style.border = "none";
+    });
+
+    
+
+    let cards = this.skillList.cards.toArray();
+    let first=-1; //top element position in array
+    let last=-1; //bottom element position in array (should be higher)
+
+    //optimisation - store these instead of calculating every scroll event (then only check before and after first)
+    cards.forEach((c, i) => {
+      if (!this.isPartiallyInViewport(c.nativeElement)) 
+        return;
+        
+      if (first == undefined || first == -1)
+        first = i;
+
+      last = i;
+    });
+
+    cards[first].nativeElement.style.border = "2px solid green";
+    cards[last].nativeElement.style.border = "2px solid blue";
+
+    if (first <= 2) {
+      //if there is 3 elements rendered on the page above it, then we are going to add more
+      const previousScroll = this.container.nativeElement.scrollTop;
+      let height = this.getRealHeight(this.skillList.cards.toArray()[0]) + this.getRealHeight(this.skillList.cards.toArray()[1]);
+        console.log(height);
+      if (this.skillList.loadNewTops()) {
+        //problem is that skillList hasn't updated yet and hence wrong numnbers - force recheck (changedetector or update array instead)
+        height = this.getRealHeight(this.skillList.cards.toArray()[0]) + this.getRealHeight(this.skillList.cards.toArray()[1]);
+        console.log(height)
+        this.container.nativeElement.scrollTop = previousScroll + height;
+        console.log(`height: ${height} \nprev: ${previousScroll}\ncur:${this.container.nativeElement.scrollTop}`);
+        console.log("add")
+        return;
+      }
+    }
+
+    if (first >= 5) {
+      const previousScroll = this.container.nativeElement.scrollTop;
+      let height = this.getRealHeight(this.skillList.cards.toArray()[0]) + this.getRealHeight(this.skillList.cards.toArray()[1]);
+      if (this.skillList.cullTop()) {
+        this.container.nativeElement.scrollTop = previousScroll - height;
+        return;
+      }
+    }
+
+    if (cards.length - 1 - last < 3) {
+      this.skillList.loadNewBottom(); //easy since it doesn't affect height
+      return;
+    }
+
+    if (cards.length - 1 - last > 3) {
+      this.skillList.cullBottom(); //easy since it doesn't affect height
+      return;
+    }
   }
 
   onSwipe(event) {
@@ -54,8 +107,6 @@ export class SkillTreeComponent implements AfterViewInit {
     if(Math.abs(event.deltaY) > 40 ) {
       if (event.deltaY < 0) {
         this.MoveDown();
-      } else {
-        //this.MoveUp();
       }
     }
   }
@@ -88,6 +139,26 @@ export class SkillTreeComponent implements AfterViewInit {
         }
       }
     }
+  }
+
+  isPartiallyInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.bottom >= 0
+    );
+  }
+
+  scrollBottom(): void {
+    try {
+      this.container.nativeElement.scrollTop = this.container.nativeElement.scrollHeight;
+    } catch (err) {}  
+  }
+
+  getRealHeight(element: any): number {
+    const styles = window.getComputedStyle(element.nativeElement);
+    const margin = parseFloat(styles['marginTop']) + parseFloat(styles['marginBottom']);
+    return margin + element.nativeElement.offsetHeight;
   }
 
 }
