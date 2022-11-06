ee8f1d8babf023db7ef20e272d82b8c317a694c7
Gravy <lleyton92@gmail.com>
2022-11-06 22:44:40 +1100

    Style and UX Improvements

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, Navigate, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import './login.scss'
 import { useGlobalState } from '../../functionality/globalState';
+
+
 import { LoadingAnimation } from '../../components/loader/loadingAnimation';
+import loginImage from '../../assets/images/Lleyton.png';
 
 const server = process.env.REACT_APP_BACKEND_SERVER ?? 'localhost';
 const port = process.env.REACT_APP_BACKEND_PORT ?? '3000';
 const protocol = process.env.REACT_APP_BACKEND_PROTOCOL ?? 'http';
-
 const apiUrl = `${protocol}://${server}:${port}/api`;
 
 //on load should check if in cookies and then skip this page
@@ -18,11 +20,34 @@ export const Login = () => {
   const [state, dispatch] = useGlobalState();
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
+  const [userIncorrect, setUserIncorrect] = useState('');
+  const [passIncorrect, setPassIncorrect] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [eyeState, setEyeState] = useState('far fa-eye-slash eyeball');
+
   const [showLoading, setShowLoading] = useState(false);
+  const [loginText, setLoading] = useState('Login'); //loading animation
+
+  useEffect(() => {
+    if (showPassword) {
+      setEyeState('far fa-eye eyeball');
+    } else {
+      setEyeState('far fa-eye-slash eyeball');
+    }
+  }, [showPassword]);
 
   const Login = async () => {
     try {
+      setPassIncorrect('');
+      setUserIncorrect('');
+      
+      if (!email || !password) {
+        alert('Please enter email and password');
+        return;
+      }
+
       setShowLoading(true);
+
       const { data } = await axios.post(`${apiUrl}/login`, {
         email,
         password
@@ -31,44 +56,62 @@ export const Login = () => {
       dispatch({ token: data.access_token, username: data.username });
       navigate(-1);
     } catch (err) {
-      if (err.response.status === 401) {
-        alert('incorrect email or password');
+      if (!err.response) {
+        alert('Network error');
         setShowLoading(false);
+        return;
       }
 
-      if (err.response.status === 408) {
-        alert('timeout');
+      if (err.response?.status === 401) {
+        console.log(err)
+        if (err.response.data === 'Authentication Failed') {
+          setPassIncorrect('incorrect');
+        }
+
+        if (err.response.data === 'User not found') {
+          setUserIncorrect('incorrect')
+        }
+
         setShowLoading(false);
       }
     }
   }
+
+  useEffect(() => {
+      const interval = setInterval(() => {
+        setLoading(loadingText => {
+          if (showLoading) {
+            const fullState = ' . . . .'
+            return loadingText.length !== fullState && loadingText !== 'Login'
+              ? loadingText + ' .'
+              : ' .'
+          } else
+            return 'Login'
+        })
+      }, 500);
+
+      return () => clearInterval(interval);
+  }, [showLoading]);
   
   return (
-    <div className='component login'>
-      <div className='center'>
-        <div className='title'>
-          <h1 className='login-title'>Gravy.cc</h1>
-          <h2 className='login-description'>Login to Gravy.cc</h2>
+    <div className='login-page-outer'>
+      <h1 className='title'>Sign in</h1>
+      <div className='login-page-inner'>
+        
+        <img className='icon' src={loginImage}/>
+        <input className={`${userIncorrect} username`} type='text' placeholder='E-mail Address' onChange={e => { setEmail(e.target.value); setUserIncorrect('')}} value={email}/>
+        <div className='pass-container'>
+          <input className={`${passIncorrect} password`} type={showPassword ? 'text' : 'password'} placeholder='Password' onChange={e => { setPassword(e.target.value); setPassIncorrect('')}} value={password}/>
+          <i className={eyeState} id="togglePassword" onClick={() => setShowPassword(current => !current)}></i>
         </div>
-        <div className='form'>
-          <button disabled className='google sso-button' onClick={() => {}}></button>
-          <button disabled className='facebook sso-button' onClick={() => {}}></button>
-          <div className="or-text-div"> 
-            <span className='or-text-span'>
-              Or
-            </span>
-          </div>
-          <input type="text" placeholder='E-mail address' name="email" onChange={e => setEmail(e.target.value)} value={email}/>
-          <input type="text" placeholder='Password' name="password" onChange={e => setPassword(e.target.value)} value={password}/>
-          <Link to={{ pathname: '/' }} className='forgot-password'>
-            <p>Forgot Password?</p>
-          </Link>
-          <button className='submit' onClick={() => Login()}>Login</button>
-          <div className='loading-animation'>
-            { showLoading && <LoadingAnimation diameter={100}></LoadingAnimation>}
-          </div>
+        
+        <div className='forgot-password-container'>
+          <button className='forgot-password-button' onClick={() => navigate(-1)}>Forgot Password?</button>
         </div>
+        
+        
       </div>
+      <button className='login-button' onClick={() => Login()}>{loginText}</button>
     </div>
   )
 }
