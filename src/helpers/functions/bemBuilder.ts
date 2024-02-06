d9d0b3223ddeb7fbc75f6d87e5f6e7a8ab88de82
Lleyton Morris <lleyton92@gmail.com>
2024-02-06 22:10:47 +1100

    Implemented BemBuilder Helper

---
+++ src/helpers/functions/bemBuilder.ts
@@ -0,0 +1,21 @@
+/**
+ * BEM builder function
+ * 
+ * The bemBuilder function is a helper function that allows you to create a BEM (Block Element Modifier) class name by providing the
+ * component name and then using the returned "base" (component name) and "generator" (function) to create the class name.
+ * 
+ * This is typed correctly and will properly infer the narrowest type possible given the inputs.
+ */
+export const bemBuilder = <T extends string>(component: T) => {
+    const generator = (
+        <GElement extends string, GModifier extends string | undefined = undefined>
+        (element: GElement, modifier?: GModifier): 
+        `${T}__${GElement}${GModifier extends undefined ? '' : `--${GModifier}`}` => 
+        `${component}__${element}${modifier ? `--${modifier}` : ''}` as any
+    )
+
+    return [
+        component,
+        generator
+    ] as const;
+}
