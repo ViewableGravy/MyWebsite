2372072bf651a714a4b1e349bbf795d815e9fdb8
Lleyton Morris <lleyton92@gmail.com>
2024-02-11 16:59:14 +1100

    ♻️Added option to pass ref to resizeObserver hook

--- src/hooks/useResizeObserver/index.tsx
+++ src/hooks/useResizeObserver/index.tsx
@@ -1,18 +1,23 @@
 import { useEffect, useRef } from "react";
 
-type TUseResizeObserver = (onResize: (entry: ResizeObserverEntry) => void) => React.MutableRefObject<HTMLDivElement | null>;
-export const useResizeObserver: TUseResizeObserver = (onResize) => {
-    const ref = useRef<HTMLDivElement>(null);
+type TUseResizeObserver = (onResize: (entry: ResizeObserverEntry) => void, ref?: React.RefObject<HTMLElement>, deps?: any[]) => React.MutableRefObject<HTMLElement | undefined>;
+export const useResizeObserver: TUseResizeObserver = (onResize, ref, deps = []) => {
+    const _ref = useRef<HTMLElement>();
+
+    useEffect(() => {
+        if (ref?.current)
+            _ref.current = ref.current;
+    }, [ref?.current])
 
     useEffect(() => {
         const observer = new ResizeObserver(([entry]) => onResize(entry));
 
-        if (ref.current) {
-            observer.observe(ref.current);
+        if (_ref.current) {
+            observer.observe(_ref.current);
         }
 
         return () => observer.disconnect();
-    }, [onResize])
+    }, [onResize, _ref.current, ...deps])
 
-    return ref;
+    return _ref;
 }
